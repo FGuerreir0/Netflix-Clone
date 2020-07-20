245c6dbfd026da6a75c4c4daeafd2e0314b8f9e4
Fabio Guerreiro <ffsguerreiro@hotmail.com>
2020-07-20 18:17:05 +0100

    Adding information to modal

--- client/src/components/Row/index.jsx
+++ client/src/components/Row/index.jsx
@@ -4,12 +4,22 @@ import './styles.css';
 import Popup from './../PopupInformation';
 import Modal from 'react-modal';
 import { AiFillCloseCircle } from 'react-icons/ai';
+import { MdStar } from 'react-icons/md';
 
 Modal.setAppElement('#root');
 
 function Row({ title, fetchUrl, isLargeRow }) {
   const [movies, setMovies] = useState([]);
   const [show, setShow] = useState(false);
+  const [showMovie, setShowMovie] = useState({});
+
+  function display(newShowMovie) {
+    if (newShowMovie !== showMovie) {
+      setShowMovie(newShowMovie);
+    }
+    setShow(true);
+    console.log(showMovie);
+  }
 
   useEffect(() => {
     async function fetchData() {
@@ -33,7 +43,7 @@ function Row({ title, fetchUrl, isLargeRow }) {
               isLargeRow ? movie.poster_path : movie.backdrop_path
             }`}
             alt={movie.name}
-            onClick={() => setShow(true)}
+            onClick={() => display(movie)}
           />
         ))}
       </div>
@@ -44,10 +54,39 @@ function Row({ title, fetchUrl, isLargeRow }) {
           className='Modal'
           overlayClassName='Overlay'
         >
-          <div style={{ cursor: 'pointer' }}>
-            <AiFillCloseCircle onClick={() => setShow(false)} size={35} color='red' />
+          <div
+            className='modal_multimedia'
+            style={{
+              backgroundImage: `url("https://image.tmdb.org/t/p/original${showMovie?.backdrop_path}")`
+            }}
+          >
+            <div className='modal_header'>
+              <h3>{showMovie?.title || showMovie?.name || showMovie?.original_name}</h3>
+              <div style={{ cursor: 'pointer' }}>
+                <AiFillCloseCircle onClick={() => setShow(false)} size={35} color='red' />
+              </div>
+            </div>
+
+            <div className='modal_content'>
+              <div className='modal_description'>
+                <p>{showMovie.overview}</p>
+                <p style={{ marginTop: '10px' }}>
+                  <strong>Released at:</strong> {showMovie.first_air_date}
+                </p>
+                <div className='modal_rating'>
+                  <p>{showMovie.vote_average} / 10 </p>
+                  <div>
+                    <MdStar size={25} />
+                  </div>
+                </div>
+              </div>
+              <div>
+                <div className='modal_trailer'>
+                  <p>{showMovie.overview}</p>
+                </div>
+              </div>
+            </div>
           </div>
-          <p>Content</p>
         </Modal>
       </div>
     </div>
