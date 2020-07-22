21c2ed0ed07678ced2380c7913668b7071e65fe3
Fabio Guerreiro <ffsguerreiro@hotmail.com>
2020-07-22 12:40:01 +0100

    added env file

--- client/src/services/apiRequest.js
+++ client/src/services/apiRequest.js
@@ -1,14 +1,12 @@
-const API_KEY = '15d1c52e906f7815bb861a6aaa13fa45';
-
 const requests = {
-  fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
-  fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
-  fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
-  fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
-  fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
-  fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
-  fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
-  fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`
+  fetchTrending: `/trending/all/week?api_key=${process.env.REACT_APP_APIKEY}&language=en-US`,
+  fetchNetflixOriginals: `/discover/tv?api_key=${process.env.REACT_APP_APIKEY}&with_networks=213`,
+  fetchTopRated: `/movie/top_rated?api_key=${process.env.REACT_APP_APIKEY}&language=en-US`,
+  fetchActionMovies: `/discover/movie?api_key=${process.env.REACT_APP_APIKEY}&with_genres=28`,
+  fetchComedyMovies: `/discover/movie?api_key=${process.env.REACT_APP_APIKEY}&with_genres=35`,
+  fetchHorrorMovies: `/discover/movie?api_key=${process.env.REACT_APP_APIKEY}&with_genres=27`,
+  fetchRomanceMovies: `/discover/movie?api_key=${process.env.REACT_APP_APIKEY}&with_genres=10749`,
+  fetchDocumentaries: `/discover/movie?api_key=${process.env.REACT_APP_APIKEY}&with_genres=99`
 };
 
 export default requests;
